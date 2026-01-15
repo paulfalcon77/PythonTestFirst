46f348fe64f2b99f2360ddf036d8cec50a6c6d97
Pavel Sokolov <pavel.sok77@gmail.com>
2026-01-15 23:02:56 +0200

    HelloWorld is correct

--- tests/qa_practice/test_input.py
+++ tests/qa_practice/test_input.py
@@ -3,7 +3,7 @@ from playwright.sync_api import Page, expect
 def test_simple_input(page: Page):
     page.goto("https://www.qa-practice.com/elements/input/simple")
     input_field = page.locator("#id_text_string")
-    input_field.fill("Hello World!")
-    input_field.press("enter")
+    input_field.fill("HelloWorld")
+    input_field.press("Enter")
     result = page.locator("#result-text")
-    expect(result).to_have_text("Hello World!")
+    expect(result).to_have_text("HelloWorld")
