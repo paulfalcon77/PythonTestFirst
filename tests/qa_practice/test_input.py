96e4284cd549ab248b2e64e8cf3c1f2a2c964fde
Pavel Sokolov <pavel.sok77@gmail.com>
2026-02-09 11:58:13 +0200

    update after 'Helloworld'

--- tests/qa_practice/test_input.py
+++ tests/qa_practice/test_input.py
@@ -7,7 +7,7 @@ def test_simple_input(page: Page):
     simple_input = InputIn(page)
     simple_input.open()
     simple_input.simple_input()
-    simple_input.check_result_text_("HelloWorld!")
+    simple_input.check_result_text_("HelloWorld")
 
 
 #update
