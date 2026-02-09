67bf32d391d70d81081a656bf055997ba0285246
Pavel Sokolov <pavel.sok77@gmail.com>
2026-02-09 11:35:56 +0200

    update after reinstall

--- tests/qa_practice/test_input.py
+++ tests/qa_practice/test_input.py
@@ -7,8 +7,8 @@ def test_simple_input(page: Page):
     simple_input = InputIn(page)
     simple_input.open()
     simple_input.simple_input()
-    simple_input.check_result_text_("HelloWorld")
+    simple_input.check_result_text_("HelloWorld!")
 
 
-
+#update
 #pytest --headed  tests/qa_practice/test_input.py
